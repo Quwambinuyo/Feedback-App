5f6f01e80093c1ffb374f9f9b86fdc2fee131fd1
Quwam Binuyo <qbinuyo7@gmail.com>
2025-01-05 11:55:29 +0100

    Generated the feedbacks from feedback data into the feedbacklist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header text="Feedback UI" />
       <div className="container">
-        <h1>My App</h1>
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    color: "#ff6a95",
+    padding: "10px 0",
+  };
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
         <h2>{text}</h2>
       </div>
