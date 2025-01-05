9978bfd9d7a47d0603ab6eca7e2f0a3daad8d81c
Quwam Binuyo <qbinuyo7@gmail.com>
2025-01-05 13:08:09 +0100

    added the rating or feedback stats to show average ratings for the overall feedbacks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -14,6 +15,7 @@ function App() {
     <>
       <Header text="Feedback UI" />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
